397644d891c1acd1a1f7a6deba2dff10b5c47552
OWEEN3 <kukilegend0@gmail.com>
2025-09-01 20:16:50 +0300

    Added responces

--- handlers/navigation.py
+++ handlers/navigation.py
@@ -7,14 +7,13 @@ from database.dao.users_dao import UsersDAO
 from services.redis_dao import RedisDAO
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(BotResponses.START)
-    
     chat_id = update.effective_user.id
     
     if not await UsersDAO.get_user(chat_id=chat_id):
         await UsersDAO.add_user(
             chat_id=chat_id
         )
+        await update.message.reply_text(BotResponses.START)
     
     job_name = f"{chat_id}"
     jobs = context.application.job_queue.get_jobs_by_name(job_name)
--- responses/response.py
+++ responses/response.py
@@ -1,5 +1,21 @@
 class BotResponses:
-    START = "Hello"
-    HELP = "Help"
-    CHOSE = "Select an action"
-    ABOUT = "About"
+    START = """Hello! I'm a bot that helps you track cryptocurrency prices. 😊
+Here's what I can do:
+📊 Send your watchlist with the latest currency rates.
+➕ Add and ➖ remove tokens from your tracking list.
+🔄 Update prices at your chosen frequency, either by sending a new message or editing the previous one.
+            
+Available commands:
+🔁 /start – Restart the bot
+🚪 /cancel – Return to the main menu
+
+All available functions are in the menu below. 👇"""
+    CHOSE = "Let me know how I can help! 🚀"
+    ABOUT = """Here's what I can do:
+📊 Send your watchlist with the latest currency rates.
+➕ Add and ➖ remove tokens from your tracking list.
+🔄 Update prices at your chosen frequency, either by sending a new message or editing the previous one.
+            
+Available commands:
+🔁 /start – Restart the bot
+🚪 /cancel – Return to the main menu"""
